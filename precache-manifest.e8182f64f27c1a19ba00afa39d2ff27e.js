86c830f4e916be07159a23eb027834ef739c8ebb
Rajan Maharjan <>
2020-03-03 22:39:18 +0545

    Updates

---
+++ precache-manifest.e8182f64f27c1a19ba00afa39d2ff27e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4332c6a32c0a204b1b5cf105a2a384a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "acb5e61e8e352fe50b70",
+    "url": "/monsters-rolodex/static/css/main.7ff973a7.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acb5e61e8e352fe50b70",
+    "url": "/monsters-rolodex/static/js/main.ef749276.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
